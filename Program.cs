2e09fb80331b3459359092ba75d227d0e0b6fd14
SVitaly <user7_490@mail.ru>
2023-02-11 14:05:02 +0300

    добавили решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                {
+                    Console.Write($"{inArray[i, j, k]} ({i},{j},{k})");
+                }
+                Console.WriteLine();
+            }
+    }
+}
+
+void FillArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                matr[i, j, k] = new Random().Next(10, 100);
+                CheckChange(matr, i, j, k);
+            }
+        }
+    }
+}
+void CheckChange(int[,,] matr, int l, int m, int n)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                if (l != i)
+                {
+                    if (m != j)
+                    {
+                        if (n != k)
+                        {
+                            if (matr[l, m, n] == matr[i, j, k])
+                            {
+                                matr[l, m, n] = new Random().Next(10, 100);
+                                CheckChange(matr, l, m, n);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+int rows = 3;
+int columns = 3;
+int lines = 3;
+int[,,] array = new int[rows, columns, lines];
+FillArray(array);
+PrintArray(array);
